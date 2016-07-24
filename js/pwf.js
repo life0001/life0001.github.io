7e4477be203a5f7cca150aeb30b3224fb36599dc
life0001 <life0001@126.com>
2016-07-24 16:09:14 +0800

    v1.0

--- js/pwf.js
+++ js/pwf.js
@@ -2,9 +2,9 @@
  * Created by Peter on 2016/7/23.
  */
 var pwf = angular.module('pwf',['ui.router']);
-pwf.controller(['$scope','$rootScope'],function($scope,$rootScope){
-    $rootScope.pwf='life0001'
-});
+pwf.controller('pwf',['$scope','$rootScope',function($scope,$rootScope){
+    $rootScope.pwf='life0001';
+}]);
 
 
 
@@ -14,12 +14,34 @@ pwf.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRo
         .state('index',{
             url:'/index',
             views:{
-                '':{
-                    templateUrl:'pwf.html'
+                'pwf':{
+                    templateUrl:'tpls/pwf.html',
+                    controller:function(){
+                        $('.navList').on('click','li',function(){
+                            $(this).addClass('cur').siblings().removeClass('cur');
+                        })
+                    }
                 },
                 'main@index':{
-                    templateUrl:'main.html'
+                    templateUrl:'tpls/main.html'
+                }
+            }
+        })
+        .state('index.project',{
+            url:'/project',
+            views:{
+                'main':{
+                    templateUrl:'tpls/project.html'
+                }
+            }
+        })
+        .state('index.writing',{
+            url:'/writing',
+            views:{
+                'main':{
+                    templateUrl:'tpls/writing.html'
                 }
             }
         })
 }]);
+
