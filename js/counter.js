cf5768bd4e6d4b33ea51fd9eefbadb46c76a2546
life0001 <life0001@126.com>
2017-03-26 00:49:41 +0800

    随机数字计算机

---
+++ js/counter.js
@@ -0,0 +1,58 @@
+(function () {
+    var $num = $('.counterLeft .num'),
+        $procedure = $('.procedure'),
+        $result = $('.result'),
+        isRepeat = true,
+        numArry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
+
+    // 输出随机数字
+    (function() {
+        for (var i = 0, len = $num.length; i < len; i++) {
+            var index = Math.floor(Math.random() * numArry.length);
+            $num.eq(i).text(numArry[index]);
+            numArry.splice(index, 1);
+        }
+    })();
+
+    // 鼠标点击事件
+    $('.counterContent').on('click', 'a', function () {
+        switch ($(this).text()) {
+            case 'C':
+                $procedure.text(0);
+                break;
+            case 'AC':
+                $procedure.text(0);
+                $result.text('');
+                break;
+            case '=':
+                try {
+                    $result.text(eval($procedure.text()));
+                } catch (err) {
+                    console.log(err);
+                }
+                break;
+            case '←':
+                if ($procedure.text().length > 0) {
+                    if ($procedure.text().length != 1) {
+                        $procedure.text($procedure.text().substr(0, $procedure.text().length - 1));
+                    } else {
+                        $procedure.text(0);
+                    }
+                }
+                isRepeat = true;
+                break;
+            default :
+                var txt = $(this).text();
+                if (txt == '+' || txt == '-' || txt == '*' || txt == '/') {
+                    if (!isRepeat) break;
+                    $procedure.text() == 0 && $procedure.text('');
+                    $procedure.text($procedure.text() + txt);
+                    isRepeat = false;
+                } else {
+                    $procedure.text() == 0 && $procedure.text('');
+                    $procedure.text($procedure.text() + txt);
+                    isRepeat = true;
+                }
+        }
+    });
+})();
